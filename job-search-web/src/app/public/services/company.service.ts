d3480928286f8b39d21f405c883080be5716413e
minlwin <lwin.zawmin@gmail.com>
2020-07-17 18:19:24 +0630

    Update Company Details

--- job-search-web/src/app/public/services/company.service.ts
+++ job-search-web/src/app/public/services/company.service.ts
@@ -9,6 +9,45 @@ export class CompanyService {
   constructor() { }
 
   findById(id: any) {
-    return of({})
+    return of({
+      id: 1,
+      name: "SOLT Engineering Co,.Ltd.",
+      logo: "job-image.png",
+      category: "Software Development",
+      founded: '2013/04',
+      employees: '12',
+      website: 'http://www.solt-e.com',
+      about: '',
+      visions: ''
+    })
+  }
+
+  findJobsForCompany(id: any, newJob: boolean) {
+    return of([
+      {
+        title: "IT Manager",
+        location: "Yangon",
+        jobType: "IT & Software Development",
+        offerDuration: "4 Days",
+        requirements: 'BEIT',
+        salary: '850000 and above.'
+      },
+      {
+        title: "Junior Java Developer",
+        location: "Yangon",
+        jobType: "Beckend Devlopment",
+        offerDuration: "4 Days",
+        requirements: 'BEIT',
+        salary: '250000 and above.'
+      },
+      {
+        title: "Front End Developer",
+        location: "Yangon",
+        jobType: "Angular and Mobile Development",
+        offerDuration: "4 Days",
+        requirements: 'BEIT',
+        salary: '350000 and above.'
+      }
+    ])
   }
 }
--- job-search-web/src/app/public/views/company/company.component.ts
+++ job-search-web/src/app/public/views/company/company.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CompanyService } from '../../services/company.service';
 
+declare let $: any
+
 @Component({
   selector: 'app-company',
   templateUrl: './company.component.html',
@@ -11,6 +13,8 @@ import { CompanyService } from '../../services/company.service';
 export class CompanyComponent implements OnInit {
 
   company: any
+  list = []
+  newJob: boolean = true
 
   constructor(private service: CompanyService, private route: ActivatedRoute) { }
 
@@ -19,6 +23,13 @@ export class CompanyComponent implements OnInit {
     console.log(id)
 
     this.service.findById(id).subscribe(data => this.company = data)
+
+    this.changeList(id, true)
+  }
+
+  changeList(id: string, newJob: boolean) {
+    this.newJob = newJob
+    this.service.findJobsForCompany(id, newJob).subscribe(result => this.list = result)
   }
 
 }
